e53487f3e8cc5f1caf83b694f5de31ef85c83386
jklenner <j@k.de>
2025-08-21 15:42:13 +0000

    changed exports_shim.c

--- pkg/exports_shim.c
+++ pkg/exports_shim.c
@@ -1,13 +1,37 @@
-#if defined(__i386__) || defined(__i386) || defined(i386)
-extern void SV_SaveGameComment(void);
-extern void Server_GetPhysicsInterface(void);
-extern void Server_GetBlendingInterface(void);
+// exports_shim.c
+// Build in the same package directory that has `import "C"`.
+// We define weak, used stubs so they are exported and not GC’d.
 
-// Make sure the linker keeps these objects and exports the symbols
-__attribute__((used, visibility("default")))
-static void *xash_export_keep[] = {
-    (void*)SV_SaveGameComment,
-    (void*)Server_GetPhysicsInterface,
-    (void*)Server_GetBlendingInterface,
-};
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+__attribute__((weak, used))
+void SV_SaveGameComment(const char *comment) {
+    (void)comment;
+}
+
+__attribute__((weak, used))
+int Server_GetPhysicsInterface(int version, void *pphysiface, void *server) {
+    (void)version; (void)pphysiface; (void)server;
+    return 0; // “not provided”
+}
+
+__attribute__((weak, used))
+int Server_GetBlendingInterface(int version, void *ppblendiface, void *server) {
+    (void)version; (void)ppblendiface; (void)server;
+    return 0; // “not provided”
+}
+
+/* Reference them so the linker definitely keeps them even with GC/LTO */
+__attribute__((used))
+static void *xash_export_keep[] = {
+    (void*)&SV_SaveGameComment,
+    (void*)&Server_GetPhysicsInterface,
+    (void*)&Server_GetBlendingInterface,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
